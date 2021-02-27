27fd2ed99787d7d4f48981c31dd2d776b1ca8bfb
Eduard <dritekop@gmail.com>
2021-02-27 14:37:33 +0200

    Changed

--- stack.c
+++ stack.c
@@ -1,29 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct stack {
-    void* ptr;
-	int top;
-	int size;
+struct stack {
+	size_t size;
+	size_t capacity;
+	int* elements;
 };
 
-struct stack create(int x) {
-    struct stack stk;
-    stk.ptr = malloc(sizeof(int)*x);
-    stk.size = x;
-    
-    return stk;
+struct stack create(size_t size, int* x) {
+	struct stack stk;
+	stk.size = size;
+	stk.capacity = size;
+	stk.elements = x;
+	
+	return stk;
 }
 
 void push(int x, struct stack* stk) {
 	stk->size += 1;
-	stk->top = x;
-	stk->ptr = realloc(stk->ptr, sizeof(int)*stk->size);	
+	int index = stk->size - 1;
+	if ( stk->size > stk->capacity ) {
+		stk->capacity = stk->size;
+		stk->elements = realloc(stk->elements, sizeof(int)*(stk->size + 1));
+	}
+	*(stk->elements + index) = x;
+}
+
+int pop(struct stack* stk) {
+	int index = stk->size - 1;
+	int element = stk->elements[index];
+	
+	return element;
+}
+
+void resize(int x, struct stack* stk) {
+	stk->capacity += x;
 }
 
 int main() {
-	struct stack stk = create(5);
-	printf("%d\n", stk.size);
-	push(4, &stk);
-	printf("%d %d\n", stk.top, stk.size);
+	int array[3] = { 10, 8, 7 };
+	
+	struct stack stk = create(3, array);
+	
+	printf("%d - stk.size %d - stk.capacity\n", stk.size, stk.capacity);
+	// printf("%d - pop(&stk), %d - stk.size\n", pop(&stk), stk.size);
+	resize(5, &stk);
+	printf("% d - stk.size %d - stk.capacity : after resize(5, &stk)\n", stk.size, stk.capacity);
+	push(123, &stk);
+	// printf("%d - pop(&stk), %d - stk.size, %d - stk.capacity\n", pop(&stk), stk.size, stk.capacity);
+	for ( ; stk.size != 0; stk.size -- ) {
+		printf("%d - pop(&stk), %d element\n", pop(&stk), stk.size);
+	}
+	printf("%d - stk.size %d - stk.capacity\n", stk.size, stk.capacity);
 }
