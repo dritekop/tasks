256e312e5df241c0ae7ff53ad7a8127c25f3128c
Eduard <dritekop@gmail.com>
2021-03-17 14:26:12 +0200

    create libraries

--- 2/fibonacciSum.c
+++ 2/fibonacciSum.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
-
-int fibonacci(int n) {
-    if ( n == 1 ) {
-        return 1;
-    }
-    if ( n > 0 ) {
-        return (fibonacci(n-1) + fibonacci(n-2));
-    }
-    if ( n < 0 ) {
-        return (fibonacci(n+2) - fibonacci(n+1));
-    }
-    return 0;
-}
-
-int fibonacciSum(int n) {
-    n += 2;
-    return fibonacci(n) - 1;
-}
+#include "fibonacciSum.h"
 
 int main() {
     FILE* test = fopen("test.txt", "w");
---
+++ 2/fibonacciSum.h
@@ -0,0 +1,19 @@
+#pragma once
+
+int fibonacci(int n) {
+    if ( n == 1 ) {
+        return 1;
+    }
+    if ( n > 0 ) {
+        return (fibonacci(n-1) + fibonacci(n-2));
+    }
+    if ( n < 0 ) {
+        return (fibonacci(n+2) - fibonacci(n+1));
+    }
+    return 0;
+}
+
+int fibonacciSum(int n) {
+    n += 2;
+    return fibonacci(n) - 1;
+}
