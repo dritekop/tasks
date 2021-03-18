1561162198634a8c7f47fef8325a31c9f14aae51
Eduard <dritekop@gmail.com>
2021-03-19 01:26:39 +0200

    grammar

--- 2/main.c
+++ 2/main.c
@@ -7,7 +7,7 @@ int main() {
     int index2;
 
     printf("Note that the values of the elements of this sequence have extremely high growing speed.\n\
-Follow the instructions and type only digits or be ready for an unexpected results.\n");
+Follow the instructions and type only digits or be ready for an unexpected result.\n");
     printf("Please type the index of a number from the Fibonacci sequence to calculate the sum of elements (only positive value): ");
     scanf("%d", &index1);
     printf("%s\n", fibonacciSumGmp(index1));
